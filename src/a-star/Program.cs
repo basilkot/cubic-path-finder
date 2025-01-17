b3703db76affd6d078bf032e0f09a3275db79db0
Basil Kotov <basilkot@gmail.com>
2025-01-17 08:07:53 +0000

    move project to bfs folder, create a* project

---
+++ src/a-star/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AStarConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
